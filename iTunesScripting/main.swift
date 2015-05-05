5df01ea96b191cc5882a4303ff9d5946da66af57
Are Digranes <aredigranes@Ares-MacBook-Pro.local>
2015-05-05 02:10:29 +0200

    Initial Commit

---
+++ iTunesScripting/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iTunesScripting
+//
+//  Created by Are Digranes on 05.05.15.
+//  Copyright (c) 2015 Are Digranes. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
